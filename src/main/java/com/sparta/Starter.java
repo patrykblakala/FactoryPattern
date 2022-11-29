3bb52667b1cacd47a7b291a99ed7959312b154a6
David <bragadireanudavid@gmail.com>
2022-11-29 17:22:38 +0000

    Changed getSpartan to createSpartan.

--- src/main/java/com/sparta/Starter.java
+++ src/main/java/com/sparta/Starter.java
@@ -3,13 +3,13 @@ package com.sparta;
 public class Starter {
     public static void start() {
         SpartanFactory spartanFactory = new SpartanFactory();
-        Spartan spartan1 = new spartanFactory.getSpartan("Java Developer");
-        Spartan spartan2 = new spartanFactory.getSpartan("Java SDET");
-        Spartan spartan3 = new spartanFactory.getSpartan("C# Developer");
-        Spartan spartan4 = new spartanFactory.getSpartan("C# SDET");
-        Spartan spartan5 = new spartanFactory.getSpartan("Data Engineer");
-        Spartan spartan6 = new spartanFactory.getSpartan("Business Analyst");
-        Spartan spartan7 = new spartanFactory.getSpartan("DevOps Engineer");
+        Spartan spartan1 = new spartanFactory.createSpartan("Java Developer");
+        Spartan spartan2 = new spartanFactory.createSpartan("Java SDET");
+        Spartan spartan3 = new spartanFactory.createSpartan("C# Developer");
+        Spartan spartan4 = new spartanFactory.createSpartan("C# SDET");
+        Spartan spartan5 = new spartanFactory.createSpartan("Data Engineer");
+        Spartan spartan6 = new spartanFactory.createSpartan("Business Analyst");
+        Spartan spartan7 = new spartanFactory.createSpartan("DevOps Engineer");
 
         System.out.println(spartan1);
         System.out.println(spartan2);
